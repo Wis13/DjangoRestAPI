5437b0f1b16c355c5c2b4ae34d2bfe9921897140
Vadym Koroliuk <vkqa@outlook.com>
2023-01-16 21:47:10 -0800

    Create application product

--- py_client/basic.py
+++ py_client/basic.py
@@ -1,8 +1,8 @@
 import requests
 
-endpoint = "http://Localhost:8000/"
+endpoint = "http://Localhost:8000/api/"
 
-get_response = requests.get(endpoint, json={"query": "Hello World"})
-print(get_response.text)
+get_response = requests.get(endpoint,params={"abs": 123},json={"query": "Hello World"})
+# print(get_response.text)
 print(get_response.status_code)
-# print(get_response.json())
+print(get_response.json())
